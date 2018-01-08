1983ff2aef5a65b94483c40857744f11206533d1
example <email@example.com>
2018-01-08 17:36:31 +0300

    save urls to localstorage

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ const app = new Vue({
   methods: {
     add() {
       if (!this.url) return;
-      const src = this.url.replace(/https?:\/\/(www\.)?youtube\.com\/watch\?v=(.+)/, 'https://www.youtube.com/embed/$2?autoplay=1');
+      const src = this.url.replace(/https?:\/\/(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)(.+)/, 'https://www.youtube.com/embed/$3?autoplay=1');
       this.streams.push({
         url: src,
       });
       this.url = '';
+      localStorage.setItem('streams', JSON.stringify(this.streams));
     },
     remove(index) {
       this.streams.splice(index, 1);
+      localStorage.setItem('streams', JSON.stringify(this.streams));
     },
+  },
+  created() {
+    const streams = localStorage.getItem('streams');
+    this.streams = streams ? JSON.parse(streams) : [];
   },
 });
