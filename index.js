408cac06cbc10baeb408e14611c474ace4a57a8c
boreich <32341756+boreich@users.noreply.github.com>
2018-12-31 22:36:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ const app = new Vue({
     url: '',
     streams: [],
   },
-  computed: {
-    inRow() {
-      const count = this.streams.length;
-      if (count <= 4) {
-        return 2;
-      } else if (count<= 6) {
-        return 3;
-      } else if (count <= 12) {
-        return 4;
-      } else {
-        return 5;
-      }
-    }
-  },
   methods: {
     add() {
       if (!this.url) return;
@@ -31,6 +17,59 @@ const app = new Vue({
     remove(index) {
       this.streams.splice(index, 1);
       localStorage.setItem('streams', JSON.stringify(this.streams));
+    },
+    itemClassMod(index) {
+      const count = this.streams.length;
+      if (count < 2) {
+        return 1;
+      }
+      if (count < 4) {
+        return 2;
+      }
+      if (count === 5) {
+        if (index <= 2) {
+          return 3;
+        } else {
+          return 2;
+        }
+      }
+      if (count === 6) {
+        return 3;
+      }
+      if (count >= 7 && count <=10 ) {
+        if (index <= 3) {
+          return 4;
+        }  else {
+          return 3;
+        }
+      }
+      if (count === 11) {
+        if (index <= 7) {
+          return 4;
+        }  else {
+          return 3;
+        }
+      }
+      if (count === 12) {
+        return 4;
+      }
+      if (count === 13) {
+        if (index <= 4) {
+          return 5;
+        } else {
+          return 4;
+        }
+      }
+      if (count >= 14 && count <= 18) {
+        if (index <= 9) {
+          return 5;
+        } else {
+          return 4;
+        }
+      }
+
+      return 5;
+      
     },
   },
   created() {
